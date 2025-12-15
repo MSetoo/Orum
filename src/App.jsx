381b8e364bb4715e9b9132a4a9a651459067cc3e
Mateo Calvache <dilan.calvache@epn.edu.ec>
2025-12-15 16:41:11 -0500

    Test: probarr conexion a supabase local y arquitectura

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { supabase } from "./api/supabaseClient";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  useEffect(() => {
+    console.log(" Probando conexión Supabase...");
+
+    supabase
+      .from("whatsapp_reports_daily") 
+      .select("id")
+      .range(0, 0)
+      .then(({ data, error }) => {
+        console.log(" DATA:", data);
+        console.log("ERROR:", error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: 40 }}>
+      <h1>Test Supabase</h1>
+      <p>Soy mateo </p>
+    </div>
+  );
 }
-
-export default App
